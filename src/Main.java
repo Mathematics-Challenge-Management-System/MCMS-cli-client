008c63f1646bf94dfa4ed1f7912a1442286fa5df
Arthur Asasira <asasiraarthur@gmail.com>
2024-07-02 22:28:43 +0300

    impliment validation by representative

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.*;
 public class Main {
     static Scanner scanner = null;
     static String choice;
-    static HashMap<String, String> participantDetails = new HashMap<>();
+//    static HashMap<String, String> participantDetails = new HashMap<>();
     static BufferedReader consoleReader = null;
     static Client client = new Client();
 //    Participant participant=new Participant();
@@ -50,21 +50,85 @@ public class Main {
              String response = client.reader.readLine();
              System.out.println(response);
              response = client.reader.readLine();
-             if(!response.equalsIgnoreCase("valid command")){
-                 System.out.println("\n----------------------------\n"+response+"\n----------------------------\n");
-                 welcome();
-             }
-             else{
-                 System.out.println("""
-                         Command sent successfully
-                         Wait for confirmation email then log in
-                         """);
 
+             switch (response) {
 
+                 case "valid register":
+                     System.out.println(response + "\n----------\nCommand sent successfully. Wait for confirmation email then log in");
+                     welcome();
+                     break;
+                 case "valid login":
+                     System.out.println(response + "\n----------\nValid login details");
+                     response = client.reader.readLine();
+                     if (response.split(" ")[0].equalsIgnoreCase("representative")) rep(response);
+                     else if (response.split(" ")[0].equalsIgnoreCase("participant")) participant(response);
+                     else {
+                         System.out.println(response + "\n----------\nInvalid command");
+                         welcome();
+                     }
+                 case "invalid":
+                     System.out.println(response + "\n----------\nInvalid command");
+                     welcome();
+                     break;
+                    default:
+                        System.out.println(response + "\n----------\nInvalid command");
+                        welcome();
+                        break;
              }
-         } catch (IOException e) {
+//             if(!response.equalsIgnoreCase("valid command")){
+//                 System.out.println("\n----------------------------\n"+response+"\n----------------------------\n");
+//                 welcome();
+//             }
+//             else {
+//                 System.out.println("\n----------------------------\n"+response+"\n----------------------------\n");
+//
+//                 System.out.println("""
+//                         Command sent successfully
+//                         Wait for confirmation email then log in
+//                         """);
+//                 if (command.strip().split(" ")[0].equalsIgnoreCase("login") {
+//                     System.out.println(client.reader.readLine());
+//                     System.out.println(client.reader.readLine());
+//                     System.out.println(client.reader.readLine());
+//                     System.out.println(client.reader.readLine());
+//                     client.printWriter.println(consoleReader.readLine());
+//                     welcome();
+//
+//
+//             }
+//         } catch (IOException e) {
+//             e.getMessage();
+//         }}
+         }catch (IOException e){
              e.getMessage();
          }
+    }
+
+    private static void participant(String response) {
+
+
+    }
+
+    private static void rep(String response) {
+
+            while ( true){
+                try {
+//                System.out.println("---------\nHello "+response.split(" ")[1]);
+                response=client.reader.readLine();
+                if (response==null)     {    welcome();break;}
+                System.out.println("\n Enter 'yes' to confirm or 'no' to reject participant details\n-----*******-------\n");
+
+                System.out.println(response);
+                choice=consoleReader.readLine();
+                client.printWriter.println(choice);
+
+
+            }catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+        }
+
     }
 
 
